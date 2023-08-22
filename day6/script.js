ce7eca415862920ab062801ca633790897e5bcb0
Manishkujire <minnu0371@gmail.com>
2023-08-22 21:28:34 +0530

    first commit

---
+++ day6/script.js
@@ -0,0 +1,9 @@
+const submit = document.getElementById("submit")
+const clear = document.getElementById("clear")
+const table = document.getElementById("mytable")
+
+const submitAction=()=>{
+    if(table.innerText==null)
+    console.log("hhh")
+    var x = document.createElement("TABLE");
+}
