361eb724d62a6fd59f2146cadd3412577097e591
Manishkujire <minnu0371@gmail.com>
2023-08-23 16:06:12 +0530

    first commit

--- day6/script.js
+++ day6/script.js
@@ -2,67 +2,149 @@ console.log("hi")
 
 
 const comment = document.getElementById('comment')
-const username = document.getElementById('name')
+const username = document.getElementById('username')
 const reg = document.getElementById('reg')
 const grade = document.getElementById('grade')
 const submit = document.getElementById('submit')
-const clear = document.getElementById('clear')
+const reset_btn = document.getElementById('reset_btn')
+const tableBody = document.getElementById('tableBody')
+let tableData = [
+    {
+        name: "manish",
+        id: 5
+    },
+    {
+        name: "caha",
+        id: 5
+    },
+    {
+        name: "zz",
+        id: 5
+    },
+    {
+        name: "bikash",
+        id: 1
+    }
+];
 
-let sl_no = 0
-let name_list = []
-let grade_list = []
-let reg_list = []
+let i = 0
 
 const submitAction = () => {
-    console.log(validate())
     if (validate()) {
-
-
-        // console.log(table.innerHTML)
-
-        if (table.innerHTML == "") {
-            let h = document.createElement("h1")
-            h.innerText = "Table"
-            h.setAttribute("class", "mb-3")
-            table.appendChild(h)
-            console.log("table created")
-            let w = document.createElement("TABLE");
-            w.setAttribute("id", "myTable");
-            // w.setAttribute("class", "table");
-            table.appendChild(w);
-            const x = document.createElement("thead");
-            const y = document.createElement("tr");
-            const z = document.createElement("th");
-            z.innerHTML = "SL_NO"
-            y.appendChild(z);
-            x.appendChild(y);
-            w.appendChild(x);
-            // z.innerHTML = "Name"
-            // y.appendChild(z);
-            // x.appendChild(y);
-            // w.appendChild(x);
-            // z.innerHTML = "Reg"
-            // y.appendChild(z);
-            // x.appendChild(y);
-            // w.appendChild(x);
-            // z.innerHTML = "Grade"
-            // y.appendChild(z);
-            // x.appendChild(y);
-            // w.appendChild(x);
-
-
+        console.log(
+            document.getElementById('username').value
+        );
+        let data = {
+            key: i,
+            name: username.value,
+            reg: reg.value,
+            grade: grade.value
         }
+        tableData.push(data)
+        console.log(
+            tableData
+        );
+        clearForm()
+        sortByName()
+        x = document.createElement('TR')
+        x.innerHTML = `<tr><td>${tableData.length}</td><td>${data.name}</td><td>${data.reg}</td><td>${data.grade}</td><td colspan="2"><div class="row g-2 "><div class="col-6 justify-content-center"><button class="btn w-100 btn-success" id="edit" onclick="editData(${i})">Edit</button></div><div class="col-6"><button onclick="deleteData(${i})" class="btn w-100 btn-danger" id="edit">Delete</button></div> </div></td></tr>`
+        tableBody.appendChild(x)
+
     }
 }
 
-const createCell=(value)={
+
+
+
+const sortByName = () => {
+    console.log(
+        tableData
+    );
+    tableData = tableData.map(
+        student => (
+            {
+                name: student.name,
+                id: student.id
+            }
+        )
+    ).sort((a, b) => a.name.localeCompare(b.name)
+    )
+
+    console.log(
+        tableData
+    );
 
 }
+const sortByReg = () => {
+    console.log(
+        tableData
+    );
+    tableData = tableData.map(
+        student => (
+            {
+                name: student.name,
+                id: student.id
+            }
+        )
+    ).sort((a, b) => a.id - b.id
+    )
+
+    console.log(
+        tableData
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const validate = () => {
     if (username.value === "" || grade.value === "" || reg.value === "") {
-        comment.innerHTML = 'Fill All the inputs';
+        if (grade.value === "") {
+            document.getElementById('gradeWarning').innerText = "The Grade field cannot be blank"
+            document.getElementById('grade').focus()
+
+        }
+        if (reg.value === "") {
+            document.getElementById('regNoWarning').innerText = "The Reg field cannot be blank"
+            document.getElementById('reg').focus()
+
+        }
+        if (username.value === "") {
+            document.getElementById('userNameWarning').innerText = "The Name field cannot be blank"
+            document.getElementById('username').focus()
+
+        }
+
+
         return false
     }
     else return true
 }
 
+const disableWarning_userName = () => {
+    userNameWarning.innerHTML = "";
+}
+const disableWarning_regNo = () => {
+    regNoWarning.innerHTML = "";
+}
+const disableWarning_grade = () => {
+    gradeWarning.innerHTML = "";
+}
+
+const clearForm = () => {
+    document.getElementById('myForm').reset()
+
+}
